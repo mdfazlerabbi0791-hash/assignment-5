718ee7307a56c63e711412ae027fa7dfb9841837
mdfazlerabbi0791-hash <mdfazlerabbi0791@gmail.com>
2026-09-12 15:11:52 +0600

    new-commit

--- src/Component/TecnologiCard/Card.tsx
+++ src/Component/TecnologiCard/Card.tsx
@@ -1,14 +1,16 @@
-import type { useState } from "react"
+import { useState } from "react"
 import type { ItechnologiesType } from "../../Type/Type"
 import YourStack from "../YourState"
+import TecnologiNewCard from "../TecnologiNewCard";
 
 export interface CardProps {
     techno: ItechnologiesType[]
 }
 
 export default function Card({ techno }: CardProps) {
-    const [yourStack, setYourSack] = useState<Technology[]>([]);
-    
+    const [yourStack, setYourSack] = useState<ItechnologiesType[]>([]);
+
+
     return (
         <>
       
@@ -20,28 +22,9 @@ export default function Card({ techno }: CardProps) {
             <div className="grid grid-cols-12 gap-4 mt-10">
                 <div className=" col-span-9 grid grid-cols-1  md:grid-cols-3 gap-6">
                 {
-                    techno.map((techno) =>{
-                        return(
-                            <div key={techno.id} className="border border-gray-200 rounded-xl p-5">
-                                <div className="flex items-center justify-between">
-                                   <img src={techno.icon} alt="icon" className="w-14 h-14 object-contain" /> 
-
-                                   <span className="border border-blue-200 bg-blue-50 text-blue-500 rounded-full px-4 py-1">{techno.badge}</span>
-
-                                </div>
-                                <h1 className="text-2xl font-bold mt-8">{techno.name}</h1>
-                                <p className="text-gray-500 text-lg leading-6 mt-4">{techno.description}</p>
-                                <div className="border-t border-gray-200 mt-6 pt-4 flex items-center justify-between">
-                                    <span className="bg-gray-100 px-3 py-2 rounded-md">{techno.category}</span>
-                                    <span className="text-gray-500">{techno.difficulty}</span>
-                                    <span className="text-gray-500"> ⭐ {techno.rating}</span>
-                                </div>
-
-                                <button className="w-full bg-gray-950 text-white rounded-xl mt-6 text-lg py-2 ">
-                                    Add to Stack</button>
-                            </div>
-                        )
-                    } )
+                    techno.map((tec) => <TecnologiNewCard tec={tec} yourStack ={yourStack} setYourSack ={setYourSack}/>)
+                        
+                    
                 }
 
                 </div>
---
+++ src/Component/TecnologiNewCard.tsx
@@ -0,0 +1,46 @@
+import type { Dispatch, SetStateAction } from "react"
+import type { ItechnologiesType } from "../Type/Type"
+
+
+export interface TecnologiNewCardProps {
+    tec: ItechnologiesType[]
+    yourStack: ItechnologiesType[]
+    setYourSack: Dispatch <SetStateAction<ItechnologiesType[]>>
+
+}
+
+export default function TecnologiNewCard({ tec, yourStack, setYourSack }: TecnologiNewCardProps) {
+
+    const handleTechno = () =>{
+        if(yourStack.includes(tec)){
+            return 
+        }else{
+            const setStack = [...yourStack, tec]
+            setYourSack(setStack)
+            
+        }
+    }
+    console.log(tec);
+    
+    return (
+
+         <div key={tec.id} className="border border-gray-200 rounded-xl p-5">
+                                <div className="flex items-center justify-between">
+                                   <img src={tec.icon} alt="icon" className="w-14 h-14 object-contain" /> 
+
+                                   <span className="border border-blue-200 bg-blue-50 text-blue-500 rounded-full px-4 py-1">{tec.badge}</span>
+
+                                </div>
+                                <h1 className="text-2xl font-bold mt-8">{tec.name}</h1>
+                                <p className="text-gray-500 text-lg leading-6 mt-4">{tec.description}</p>
+                                <div className="border-t border-gray-200 mt-6 pt-4 flex items-center justify-between">
+                                    <span className="bg-gray-100 px-3 py-2 rounded-md">{tec.category}</span>
+                                    <span className="text-gray-500">{tec.difficulty}</span>
+                                    <span className="text-gray-500"> ⭐ {tec.rating}</span>
+                                </div>
+
+                                <button className="w-full bg-gray-950 text-white rounded-xl mt-6 text-lg py-2" onClick={handleTechno}>
+                                    Add to Stack</button>
+                            </div>
+    )
+}
