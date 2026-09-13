1d91efbfa91aece4fc66300b91a23279a1bbc907
mdfazlerabbi0791-hash <mdfazlerabbi0791@gmail.com>
2026-09-13 15:09:38 +0600

    all-project

--- src/Component/Navber.tsx
+++ src/Component/Navber.tsx
@@ -18,7 +18,7 @@ export default function Navber() {
                 <li className='cursor-pointer hover:text-[#d91b7e]'>Contact</li>
             </ul>
 
-            <div className='flex gap-3'>
+            <div className=' pr-5 md:pr-0 flex gap-3'>
                 <button className='border-none cursor-pointer'>Sign in</button>
                 <button className='bg-[#d91b7e] text-white py-2 px-4 rounded-3xl cursor-pointer'>Sign Up</button>
             </div>
